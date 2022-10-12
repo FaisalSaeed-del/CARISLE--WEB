c27fa95c0bc676f8e657f9984bcd3e427d024bab
FaisalSaeed-del <shoaibchudhary678@gmail.com>
2022-10-12 13:32:35 +0500

    message

--- src/components/brand/Brands.js
+++ src/components/brand/Brands.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import B1 from "../assets/images/brand1.png";
 import B2 from "../assets/images/brand2.png";
 import B3 from "../assets/images/brand3.png";
@@ -10,56 +9,52 @@ import b3 from "../assets/images/b3.png";
 import b4 from "../assets/images/b4.png";
 
 const Brands = () => {
-  // const [isHovering, setIsHovering] = useState(false);
-
-  // const handleMouseOver = () => {
-  //   setIsHovering(true);
-  // };
-
-  // const handleMouseOut = () => {
-  //   setIsHovering(false);
-  // };
-
-  // return (
-  //   <div>
-  //     <div>
-  //       <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-  //         Hover me
-  //       </div>
-
-  //       {isHovering && <h2>Hello world</h2>}
-  //     </div>
-  //   </div>
-  // );
-
-  // Map
+  const [brands, setBrands] = useState({
+    id: "1",
+    name: "HERTIX",
+    Image: B1,
+    Image2: b1,
+    para: "RESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
+  });
 
   const Branddata = [
     {
       id: "1",
       Image: B1,
+      name: "Hertix",
       Image2: b1,
       para: "RESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
     },
     {
       id: "2",
       Image: B2,
+      name: "Aertix",
       Image2: b2,
-      para: "RESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
+      para: "HESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
     },
     {
       id: "3",
       Image: B3,
+      name: "Lertix",
       Image2: b3,
-      para: "RESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
+      para: "ESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
     },
     {
       id: "4",
       Image: B4,
+      name: "Lertix",
       Image2: b4,
-      para: "RESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
+      para: "LESITRIX® ist die einzige Dichtungsbahn, die in einer beispiellosen Materialkombination aus dem Synthesekautschuk EPDM und hochwertigem polymermodifizierten Bitumen die Vorteile beider Werkstoffe vereint. Lorem ipsum dolor sit amet...",
     },
   ];
+  const Mouseover = (detail) => {
+    setBrands({
+      id: detail.id,
+      Image: detail.Image,
+      para: detail.para,
+      name: detail.name,
+    });
+  };
 
   return (
     <div>
@@ -73,22 +68,50 @@ const Brands = () => {
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-4 p-10 gap-6">
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 p-10 gap-6">
           {Branddata.map((detail) => {
             return (
-              <div className="show-hide">
+              <div className="">
                 <div>
-                  <img src={detail.Image} alt="" />
+                  <img
+                    src={detail.Image}
+                    onMouseOver={() => Mouseover(detail)}
+                    alt=""
+                  />
                 </div>
                 <div className="relative left-5 bottom-12">
                   <img src={detail.Image2} alt />
                 </div>
-                <div className=" para show-hide text-justify p-1">
-                  <p>{detail.para}</p>
-                </div>
               </div>
             );
           })}
+        </div>
+
+        {/* Details Section */}
+
+        <div className=" clip py-14 px-36">
+          <div className=" bg-white ">
+            <div className="bg-gray-100">
+              <div className="py-20 px-5 flex flex-row justify-between">
+                <div>
+                  <img src={brands.Image} alt="" />
+                </div>
+                <div className="font-roboto">
+                  <h1 className="text-[24px] mt-[38px] text-blue-900 mb-[20px] ">
+                    {brands.name}
+                  </h1>
+                  <div className=" max-w-[550px] text-[16px]">
+                    <p>{brands.para}</p>
+                  </div>
+                  <div className="py-10">
+                    <button className="sol-btn text-white text-[14px]">
+                      MORE DETAILS
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,44 +1,10 @@
 import React from "react";
-import BOX7 from "../assets/images/details.png";
+// import BOX7 from "../assets/images/details.png";
 
-const Details = () => {
+const Details = (props) => {
   return (
     <div className="bg-white">
-      <section className="container">
-        <div className=" py-10 px-36">
-          <div className=" bg-white ">
-            <div className="shadow-md shadow-black  bg-gray-300">
-              <div
-                className="py-10 px-5 flex
-               flex-row flex-wrap space-x-[44px]"
-              >
-                <div>
-                  <img src={BOX7} alt="" />
-                </div>
-                <div className="font-roboto">
-                  <h1 className="text-[24px] mt-[38px] text-blue-900 mb-[20px] ">
-                    Das Multitalent für jedes Dach
-                  </h1>
-                  <div className="text-justify md:max-w-[550px] text-[16px]">
-                    <p>
-                      RESITRIX® ist die einzige Dichtungsbahn, die in einer
-                      beispiellosen Materialkombination aus dem
-                      Synthesekautschuk EPDM und hochwertigem
-                      polymermodifizierten Bitumen die Vorteile beider
-                      Werkstoffe vereint. Lorem ipsum dolor sit amet...
-                    </p>
-                  </div>
-                  <div className="py-10">
-                    <button className="sol-btn text-white text-[14px]">
-                      MORE DETAILS
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <section className="container"></section>
     </div>
   );
 };
